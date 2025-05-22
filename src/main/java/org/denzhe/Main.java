e5c6a0863009afce7150b8725a0c0109803ae5da
denzhe dzebu <dedzebujhb024@student.wethinkcode.co.za>
2025-05-22 12:01:09 +0200

    added file reading

--- src/main/java/org/denzhe/Main.java
+++ src/main/java/org/denzhe/Main.java
@@ -1,7 +1,10 @@
 package org.denzhe;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(new GenerateQuestions("mean Software developer"));
+    public static void main(String[] args) throws FileNotFoundException {
+
+        System.out.println(new GenerateQuestions(ReadFile.readFile()));
     }
 }
---
+++ src/main/java/org/denzhe/ReadFile.java
@@ -0,0 +1,15 @@
+package org.denzhe;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static String readFile() throws FileNotFoundException{
+
+    return new File("src/main/java/org/denzhe/jobpost.txt").toString();
+    }
+}
+
+
